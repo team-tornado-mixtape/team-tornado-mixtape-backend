41ed237b6dad816b39226e6f39bf81211cfb90ee
Shawna Cooper <scooper1920@gmail.com>
2022-06-21 13:34:06 -0400

    (added social optional social stuff, added image field to profile, deleted third party info from profile attributes)

--- api/models.py
+++ api/models.py
@@ -13,31 +13,32 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    spotify_username = models.CharField(default='', max_length=255)
-    spotify_password = models.CharField(default='', max_length=255)
-    spotify_created_at = models.DateTimeField()
-    apple_music_username = models.CharField(default='', max_length=255)
-    apple_music_password = models.CharField(default='', max_length=255)
-    apple_music_created_at = models.DateTimeField()
-    followers = models.ManyToManyField(User, related_name='followers')
+    user        = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    followers   = models.ManyToManyField(User, related_name='followers')
+    image       = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
 
     def follower_count(self):
         return self.followers.count()
 
+# The following lines of code can be added to add functionality to the connection model.
+    # def get_connections(self):
+    #     connections = Connection.objects.filter(creator=self.user)
+    #     return connections
 
-class FriendRequest(models.Model):
-    pass
+    # def get_followers(self):
+    #     followers   = Connection.objects.filter(following=self.user)
+    #     return followers
+
 
 
 class Mixtape(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(User, related_name='creator', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    is_public = models.BooleanField(default=False)
-    description = models.TextField()
-    modified_at = models.DateTimeField(auto_now=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    creator      = models.ForeignKey(User, related_name='creator', on_delete=models.CASCADE)
+    title        = models.CharField(max_length=255)
+    is_public    = models.BooleanField(default=False)
+    description  = models.TextField()
+    modified_at  = models.DateTimeField(auto_now=True)
     favorited_by = models.ManyToManyField(User, related_name='favorite_mixtapes', blank=True)
 
     def favorite_count(self):
@@ -51,18 +52,18 @@ class Mixtape(models.Model):
 
 
 class Song(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    spotify_title = models.TextField()
-    spotify_artist = models.TextField()
-    spotify_album = models.TextField()
-    spotify_url = models.TextField()
-    apple_music_title = models.TextField()
-    apple_music_artist = models.TextField()
-    apple_music_album = models.TextField()
-    apple_music_url = models.TextField()
+    created_at          = models.DateTimeField(auto_now_add=True)
+    spotify_title       = models.TextField()
+    spotify_artist      = models.TextField()
+    spotify_album       = models.TextField()
+    spotify_url         = models.TextField()
+    apple_music_title   = models.TextField()
+    apple_music_artist  = models.TextField()
+    apple_music_album   = models.TextField()
+    apple_music_url     = models.TextField()
     spotify_preview_url = models.TextField()
-    mixtapes = models.ManyToManyField(Mixtape, related_name='mixtapes', blank=True)
-    favorited_by = models.ManyToManyField(User, related_name='favorite_songs')
+    mixtapes            = models.ManyToManyField(Mixtape, related_name='mixtapes', blank=True)
+    favorited_by        = models.ManyToManyField(User, related_name='favorite_songs')
 
     def favorite_count(self):
         return self.favorited_by.count()
@@ -72,3 +73,11 @@ class Song(models.Model):
 
     def __str__(self):
         return self.spotify_title
+
+# the following model can be added to create connections between users
+# class Connection(models.Model):
+#     created   = models.DateTimeField(auto_now_add=True, editable=False)
+#     creator   = models.ForeignKey(User, related_name="friendship_creator_set")
+#     following = models.ForeignKey(User, related_name="friend_set")
+
+
