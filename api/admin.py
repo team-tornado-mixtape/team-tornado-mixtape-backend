798fd9fbe249cb2fcbc834bafe972bc2d9000704
Shawna Cooper <scooper1920@gmail.com>
2022-06-21 13:14:45 -0400

    (serializers added for Mixtape, registered models in admin)

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile, Mixtape, Song
 
 admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Mixtape)
+admin.site.register(Song)
