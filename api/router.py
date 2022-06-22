b4a0d0040da753a33e37b02121b12e39e3339084
Shawna Cooper <scooper1920@gmail.com>
2022-06-22 11:51:19 -0400

    (nested router)

--- api/router.py
+++ api/router.py
@@ -6,4 +6,10 @@ from api import views as api_views
 
 router = routers.DefaultRouter()
 router.register('mixtapes',api_views.MixtapeViewSet)
-router.register('users',api_views.UserViewSet)
+router.register('users',api_views.UserViewSet,basename='users')
+users_router = routers.NestedSimpleRouter(router,'users', lookup='user')
+users_router.register(
+    'profiles',
+    api_views.ProfileView,
+    basename='profiles',
+)
