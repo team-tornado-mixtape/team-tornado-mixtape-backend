9fc8f59072024ef5e6bde325970a930e8921151a
Gerardo Ayes <gerardoayes92@gmail.com>
2022-06-22 10:27:21 -0400

    spotify search done as a callable function

--- api/spotify_search.py
+++ api/spotify_search.py
@@ -1,42 +1,20 @@
-# import spotipy
-# from spotipy.oauth2 import SpotifyOAuth
-# import json
-
-# scope ='playlist-modify-public'
-# username ='22yw7ak2iuqbzhlkprlbcbnka'
-
-# token = SpotifyOAuth(scope=scope,username=username)
-# spotifyObject = spotipy.Spotify(auth_manager=token)
-
-# create mixtape. using "playlist" because i think it may be spotify terminology
-# playlist_name= input("Enter Mixtape Name: ")
-# playlist_description= input("Enter Mixtape Description: ")
-
-# spotifyObject.user_playlist_create(user=username,name=playlist_name,public=True, description=playlist_description)
-
-# user_input = input("Enter Song Title: ")
-# list_of_songs = []
-
-# while user_input != 'quit':
-#     result = spotifyObject.search(q=user_input)
-#     print(result)
-#     print(json.dumps(result,sort_keys=4,indent=4))
-#     print(result['tracks']['items'][0]['uri'])
-#     list_of_songs.append(result['tracks']['items'][0]['uri'])
-#     user_input = input("Enter Song Title: ")
-
-# prePlaylist = spotifyObject.user_playlists(user=username)
-# playlist = prePlaylist['items'][0]['id']
-
-# add songs
-# spotifyObject.user_playlist_add_tracks(user=username,playlist_id=playlist,tracks=list_of_songs)
-
-
-
 import base64
 import requests
 import json
 from urllib.parse import urlencode
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+client_id = env('spotify_client_id')
+client_secret = env('spotify_client_secret')
 
 
 class SpotifyAPI:
@@ -67,35 +45,34 @@ class SpotifyAPI:
         self.access_token = access_token
         return token_response_data
 
+def SearchSpotifyAPI(search, limit=10):
+    spotify_client = SpotifyAPI(client_id, client_secret)
+    access_token = spotify_client.access_token
 
-client_id = '1e6071d60ecc4bcaa9e077b9be068df2'
-client_secret = 'fbd694a6c13048059bc032386f5eee36'
+    headers = {'Authorization': f"Bearer {access_token}"}
+    endpoint = "https://api.spotify.com/v1/search"
 
-spotify_client = SpotifyAPI(client_id, client_secret)
-access_token = spotify_client.access_token
+    data = urlencode({
+        "q": f"{search}",
+        "type": "track",
+        "limit": f"{limit}"
+    })
 
-headers = {'Authorization': f"Bearer {access_token}"}
+    lookup_url = f"{endpoint}?{data}"
+    req = requests.get(lookup_url, headers=headers)
 
-endpoint = "https://api.spotify.com/v1/search"
+    results = []
 
-search = input("Search terms: ")
-limit = input("limit: ")
-data = urlencode({
-    "q": f"{search}",
-    "type": "track",
-    "limit": f"{limit}"
-})
+    for i in range(len(req.json()['tracks']['items'])):
+        result = {"spotify_id": req.json()['tracks']['items'][i]['id']}
+        result["spotify_title"] = req.json()['tracks']['items'][i]['name']
+        result["spotify_album"] = req.json()['tracks']['items'][i]['album']['name']
+        result["spotify_artist"] = req.json()['tracks']['items'][i]['artists'][0]['name']
+        result["spotify_uri"] = req.json()['tracks']['items'][i]['uri']
+        results.append(result)
 
-lookup_url = f"{endpoint}?{data}"
+    return results
 
-req = requests.get(lookup_url, headers=headers)
 
-for i in range(len(req.json()['tracks']['items'])):
-    print("spotify_id: ", req.json()['tracks']['items'][i]['id'])
-    print("spotify_title: ", req.json()['tracks']['items'][i]['name'])
-    print("spotify_album: ", req.json()['tracks']['items'][i]['album']['name'])
-    print("spotify_artist: ", req.json()['tracks']['items'][i]['artists'][0]['name'])
-    print("spotify_uri: ", req.json()['tracks']['items'][i]['uri'], '\n')
-
-# print(json.dumps(req.json(), sort_keys=4, indent=4)['tracks']['items'])
-# print(req.json()['tracks']['items'][0]['uri'])
+spotify_search_results = SearchSpotifyAPI('Enter Galactic')
+print(spotify_search_results)
