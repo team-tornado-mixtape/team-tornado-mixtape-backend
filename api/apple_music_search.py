442a7a209a32bbb81ec2a0584bf14e31ee560d0a
Gerardo Ayes <gerardoayes92@gmail.com>
2022-06-22 12:04:07 -0400

    apple music search works

--- api/apple_music_search.py
+++ api/apple_music_search.py
@@ -47,7 +47,7 @@ def apple_music_search(search, limit=10):
     token = jwt.encode(payload, secret, algorithm=alg, headers=headers)
 
     data = urlencode({
-        "types": "songs,artists",
+        "types": "songs",
         "term": f"{search}",
         "limit": f"{limit}"
         })
@@ -56,7 +56,19 @@ def apple_music_search(search, limit=10):
 
     req = requests.get(url, headers={'Authorization': "Bearer " + token})
 
-    return req.json()
+    results = []
 
-search_results = apple_music_search("Enter Galactic", limit=1)
-print(search_results)
+    for i in range(len(req.json()['results']['songs']['data'])):
+        result = {"apple_id": req.json()['results']['songs']['data'][i]['id']}
+        result["apple_title"] = req.json()['results']['songs']['data'][i]['attributes']['name']
+        result["apple_album"] = req.json()['results']['songs']['data'][i]['attributes']['albumName']
+        result["apple_artist"] = req.json()['results']['songs']['data'][i]['attributes']['artistName']
+        result["apple_url"] = req.json()['results']['songs']['data'][i]['attributes']['url']
+        results.append(result)
+
+    # print(json.dumps(req.json(), sort_keys=4,indent=4))
+    return results
+
+
+apple_search_results = apple_music_search("Enter Galactic", limit=3)
+print(apple_search_results)
