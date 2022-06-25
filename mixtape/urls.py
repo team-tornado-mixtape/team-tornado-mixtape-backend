134c55320e17e9d2d6260f8017d03f7c2b2c3c2c
Shawna Cooper <scooper1920@gmail.com>
2022-06-25 13:59:01 -0400

    (urls)

--- mixtape/urls.py
+++ mixtape/urls.py
@@ -14,11 +14,16 @@ urlpatterns = [
 
     path('api/', include(router.urls)),
     path('api/my/mixtapes', api_views.UserMixtapeListView.as_view(), name='user_mixtapes'),
+    path('api/my/mixtapes/', api_views.UserMixtapeListView.as_view(), name='user_mixtapes'),
     path('api/my/profile', api_views.UserProfileView.as_view(),name='user_profile'),
+    path('api/my/profile/', api_views.UserProfileView.as_view(),name='user_profile'),
     path('api/profiles/<int:profile_pk>/following', api_views.CreateUpdateFollowingView.as_view(),name ='create_following'),
     path('api/mixtapes/<int:mixtape_pk>/favorites', api_views.CreateUpdateFavoriteView.as_view(),name ='create_favorites'),
     path('api/my/favorites',api_views.FavoriteMixtapeListView.as_view(),name='list_favorites'),
+    path('api/my/favorites/',api_views.FavoriteMixtapeListView.as_view(),name='list_favorites'),
     path('api/following',api_views.UserFollowingView.as_view(),name='user_following'),
+    path('api/following/',api_views.UserFollowingView.as_view(),name='user_following'),
     path('api/followers',api_views.UserFollowersView.as_view(),name='user_followers'),
+    path('api/followers/',api_views.UserFollowersView.as_view(),name='user_followers'),
     path('api/search', api_views.SearchView.as_view(), name='search'),
 ]
