f911013bcf8d4d8aa739ce6e105c8fa81067a49c
Gerardo Ayes <gerardoayes92@gmail.com>
2022-06-28 12:20:02 -0400

    I might have something by making the songs displayed required to be exact matches for title and artist

--- api/helpers.py
+++ api/helpers.py
@@ -10,6 +10,7 @@ def similar(a, b):
 def my_search(search_track=None, search_artist=None, limit=20):
     spotify_results = SearchSpotifyAPI(search_track=search_track, search_artist=search_artist, limit=limit)
     apple_results = SearchAppleMusicAPI(search_track=search_track, search_artist=search_artist, limit=limit)
+
     songs = []
     spotify_ids = {}
     apple_ids = {}
@@ -24,7 +25,7 @@ def my_search(search_track=None, search_artist=None, limit=20):
         closest = max(similarities)
         index = similarities.index(closest)
 
-        if closest > 1.6 and spotify_results[index]["spotify_id"] not in spotify_ids and apple_results[index]["apple_id"] not in apple_ids:
+        if closest == 2 and spotify_results[index]["spotify_id"] not in spotify_ids and apple_results[index]["apple_id"] not in apple_ids:
             song = {
                 "title": apple_results[index]["apple_title"],
                 "artist": apple_results[index]["apple_artist"],
@@ -36,7 +37,7 @@ def my_search(search_track=None, search_artist=None, limit=20):
                 }
 
             spotify_ids[song['spotify_id']] = 1
-            spotify_ids[song['apple_id']] = 1
+            apple_ids[song['apple_id']] = 1
             songs.append(song)
 
     return songs
